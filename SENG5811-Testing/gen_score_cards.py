8d27d870c37e4498130b96f367e70b7a611822ed
Stephen Stack <Stephen.Stack@etherios.com>
2015-04-06 20:34:24 -0500

    SENG5811: run golfscore against each input file

--- SENG5811-Testing/gen_score_cards.py
+++ SENG5811-Testing/gen_score_cards.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import random
 import os
+import shlex
+import subprocess
+import time
+import shutil
 
 THIS_DIR = os.path.dirname(__file__)
 INPUT_FILE_DIR = os.path.join(THIS_DIR, 'input_files')
@@ -8,6 +12,8 @@ OUTPUT_FILE_DIR = os.path.join(THIS_DIR, 'output_files')
 GOLFER_REPORT = os.path.join(os.getcwd(), 'golfer.rep')
 COURSE_REPORT = os.path.join(os.getcwd(), 'course.rep')
 TOURNAMENT_REPORT = os.path.join(os.getcwd(), 'trank.rep')
+GOLFSCORE_BIN = os.path.join(THIS_DIR, 'golf_win32')
+
 
 PROG_INPUT = {
     'courses': {
@@ -53,9 +59,26 @@ def _gen_score_value():
     return random.randint(1, 9)
 
 
+def _clean_reports():
+    if os.path.exists(GOLFER_REPORT):
+        print "Cleaning golfer report..."
+        os.remove(GOLFER_REPORT)
+
+    if os.path.exists(COURSE_REPORT):
+        print "Cleaning course report..."
+        os.remove(COURSE_REPORT)
+
+    if os.path.exists(TOURNAMENT_REPORT):
+        print "Cleaning tournament report..."
+        os.remove(TOURNAMENT_REPORT)
+
+
 def _gen_input_files(number):
+    if not os.path.exists(INPUT_FILE_DIR):
+        os.makedirs(INPUT_FILE_DIR)
+
     for no in xrange(number):
-        input_filename = "input_files/in_golf_scores_{}.txt".format(no)
+        input_filename = os.path.join(INPUT_FILE_DIR, "in_golf_scores_{}.txt".format(no))
         with open(input_filename, 'w') as f:
 
             # write courses
@@ -81,22 +104,28 @@ def _gen_input_files(number):
             f.write('_\n')
 
 
-def _clean_reports():
-    if os.path.exists(GOLFER_REPORT):
-        print "Cleaning golfer report..."
-        os.remove(GOLFER_REPORT)
+def _gen_output_files():
+    root, dirs, files = os.walk(INPUT_FILE_DIR).next()
+    for i, input_file in enumerate(files):
+        input_file_path = os.path.join(root, input_file)
+        command = "{} {} {}".format(GOLFSCORE_BIN, '-g', os.path.abspath(input_file_path))
+        print "Running golf score against {}...".format(input_file)
+        subprocess.check_output(shlex.split(command))
+        output_file_path = os.path.join(OUTPUT_FILE_DIR, 'golf_report_{}.txt'.format(i))
+        os.rename(GOLFER_REPORT, output_file_path)
+        print "Generated {}...".format(output_file_path)
+        _clean_reports()
 
-    if os.path.exists(COURSE_REPORT):
-        print "Cleaning course report..."
-        os.remove(COURSE_REPORT)
 
-    if os.path.exists(TOURNAMENT_REPORT):
-        print "Cleaning tournament report..."
-        os.remove(TOURNAMENT_REPORT)
+def _verify_requirements():
+    pass  # TODO: implement me
+
 
 def main():
     _clean_reports()
     _gen_input_files(10)
+    _gen_output_files()
+    _verify_requirements()
 
 
 if __name__ == "__main__":
